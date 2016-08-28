9f5a64e3be3cdfa45dd0871eec282960f58e2548
sewa312 <sewa080@mail.ru>
2016-08-29 01:06:48 +0300

    Bookstore with different controllers

---
+++ BookStore/Controllers/BookShopController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookShopController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
